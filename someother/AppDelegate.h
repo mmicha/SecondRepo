47f6c6d0a7f2cdf856ca2b2ffafb5ab3d573b101
Mikhail <m.baykov@gmail.com>
2012-11-04 17:31:16 +0400

    Initial Commit

---
+++ someother/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  someother
+//
+//  Created by Michael on 04.11.12.
+//  Copyright (c) 2012 mikeapphouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ someother/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  someother
+//
+//  Created by Michael on 04.11.12.
+//  Copyright (c) 2012 mikeapphouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
